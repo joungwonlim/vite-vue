1ae751e406ee435cf48d9a158b6f4d6c9333566d
joungwonlim <joungwonlim@gmail.com>
2022-10-21 22:27:12 +0900

    vite vue typescript

---
+++ vite-vue-typescript/src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const todos = ref(['about', 'till']);
+</script>
+<template>
+  <div class="about">
+    <div class="container-fluid">
+      <ul>
+        <li v-for="(todo, index) in todos" :key="index">
+          {{ todo }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style lang="sass" scoped></style>
